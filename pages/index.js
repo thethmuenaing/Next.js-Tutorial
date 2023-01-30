6e1935f8688accf5be7939816f1f8af36e0a6efd
thethmuenaing <thethmuenaing22112002@gmail.com>
2023-01-30 22:30:56 +0630

    Navigating Programmatically

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,12 @@
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function Home() {
+	const router = useRouter();
+	const handleClick = () => {
+		console.log("Placing your order");
+		router.push("/product");
+	};
 	return (
 		<div>
 			<h1>Home Page</h1>
@@ -8,6 +14,8 @@ function Home() {
 			<Link href="/blog">Blog</Link>
 			<br />
 			<Link href="/product">Product</Link>
+			<br />
+			<button onClick={handleClick}>Place Order</button>
 		</div>
 	);
 }
