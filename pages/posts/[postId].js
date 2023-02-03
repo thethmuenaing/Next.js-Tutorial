e2a265de36ea32c7a281fd3f333739cbf42fef90
thethmuenaing <thethmuenaing22112002@gmail.com>
2023-02-03 22:55:29 +0630

    getStaticPaths fallback true

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -1,4 +1,11 @@
+import { useRouter } from "next/router";
+
 function Post({ post }) {
+	const router = useRouter();
+
+	if (router.isFallback) {
+		return <h1>Loading...</h1>;
+	}
 	return (
 		<>
 			<h2>
@@ -11,36 +18,36 @@ function Post({ post }) {
 export default Post;
 
 export async function getStaticPaths() {
-	const response = await fetch("https://jsonplaceholder.typicode.com/posts");
-	const data = await response.json();
+	// const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+	// const data = await response.json();
 
-	const paths = data.map((post) => {
-		return {
-			params: {
-				postId: `${post.id}`,
-			},
-		};
-	});
+	// const paths = data.map((post) => {
+	// 	return {
+	// 		params: {
+	// 			postId: `${post.id}`,
+	// 		},
+	// 	};
+	// });
 	return {
-		// paths: [
-		// 	{
-		// 		params: {
-		// 			postId: "1",
-		// 		},
-		// 	},
-		// 	{
-		// 		params: {
-		// 			postId: "2",
-		// 		},
-		// 	},
-		// 	{
-		// 		params: {
-		// 			postId: "3",
-		// 		},
-		// 	},
-		// ],
-		paths,
-		fallback: false,
+		paths: [
+			{
+				params: {
+					postId: "1",
+				},
+			},
+			{
+				params: {
+					postId: "2",
+				},
+			},
+			{
+				params: {
+					postId: "3",
+				},
+			},
+		],
+		// paths,
+		fallback: true,
 	};
 }
 
@@ -51,6 +58,14 @@ export async function getStaticProps(context) {
 	);
 
 	const data = await response.json();
+
+	if (!data.id) {
+		return {
+			notFound: true,
+		};
+	}
+
+	console.log(`Generating page for /posts/${params.postId}`);
 
 	return {
 		props: {
