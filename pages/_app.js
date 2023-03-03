02eba69b7fbb23beaa448914c30df121bd397885
thethmuenaing <thethmuenaing22112002@gmail.com>
2023-03-03 10:49:22 +0630

    App Layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,7 @@
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+
+import "@/styles/layout.css";
 import "@/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ThemeProvider } from "styled-components";
@@ -9,9 +13,15 @@ const theme = {
 };
 
 export default function App({ Component, pageProps }) {
+	if (Component.getLayout) {
+		return Component.getLayout(<Component {...pageProps} />);
+	}
+
 	return (
 		<ThemeProvider theme={theme}>
+			<Header />
 			<Component {...pageProps} />
+			<Footer />
 		</ThemeProvider>
 	);
 }
--- pages/about.js
+++ pages/about.js
@@ -1,10 +1,21 @@
+import Footer from "../components/Footer";
+
 import styles from "../styles/About.module.scss";
 function About() {
 	return (
-		<>
+		<div className="content">
 			<button className="btn btn-primary">Primary</button>
 			<div className={styles.highlightscss}>About Page</div>
-		</>
+		</div>
 	);
 }
 export default About;
+
+About.getLayout = function PageLayout(page) {
+	return (
+		<>
+			{page}
+			<Footer />
+		</>
+	);
+};
