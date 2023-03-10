c31408bf0da62ddc1649a81102684f8a8e58e387
thethmuenaing <thethmuenaing22112002@gmail.com>
2023-03-10 14:09:28 +0630

    Securing Pages Client-side

--- next-auth/pages/dashboard.js
+++ next-auth/pages/dashboard.js
@@ -1,4 +1,28 @@
+// import { useSession } from "next-auth/client";
+import { getSession, signIn } from "next-auth/client";
+import { useState, useEffect } from "react";
+
 function Dashboard() {
+	// const [session, loading] = useSession();
+	// console.log({ session, loading });
+	const [loading, setLoading] = useState(true);
+
+	useEffect(() => {
+		const securePage = async () => {
+			const session = await getSession();
+			if (!session) {
+				signIn();
+			} else {
+				setLoading(false);
+			}
+		};
+		securePage();
+	}, []);
+
+	if (loading) {
+		return <h2>Loading...</h2>;
+	}
+
 	return <h1>Dashboard pae</h1>;
 }
 export default Dashboard;
