a6acfa41555108ce369bdba36e31c89be3cdd83b
thethmuenaing <thethmuenaing22112002@gmail.com>
2023-03-14 00:07:28 +0630

    Server-Side Authentication

--- next-auth/components/Navbar.js
+++ next-auth/components/Navbar.js
@@ -9,7 +9,8 @@ function Navbar() {
 			<h1 className="logo">
 				<a href="#">NextAuth</a>
 			</h1>
-			<ul className={`main-nav ${!session && loading ? "loading" : "loaded"} `}>
+			{/* ${!session && loading ? "loading" : "loaded"} */}
+			<ul className={`main-nav`}>
 				<li>
 					<Link href="/">Home</Link>
 				</li>
--- next-auth/pages/blog.js
+++ next-auth/pages/blog.js
@@ -1,4 +1,18 @@
-function Blog() {
-	return <h1>Blog Page</h1>;
+import { getSession, useSession } from "next-auth/client";
+
+function Blog({ data }) {
+	const [session] = useSession();
+	console.log({ session });
+	return <h1>Blog Page - {data}</h1>;
 }
 export default Blog;
+
+export async function getServerSideProps(context) {
+	const session = await getSession(context);
+	return {
+		props: {
+			session,
+			data: session ? "List of 100 perzonalized blogs" : "List of free blogs",
+		},
+	};
+}
